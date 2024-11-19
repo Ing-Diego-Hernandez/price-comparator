8786af90554b0a3d6fee5b12d486c586727db723
Diego <diegomaximiliano1203@gmail.com>
2024-11-19 03:18:59 -0600

    commit

--- src/scrapingServer.js
+++ src/scrapingServer.js
@@ -20,10 +20,10 @@ app.get('/scrape', async (req, res) => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process', '--no-zygote'], // Necesario para entornos virtualizados como Render
-    executablePath: 
+    /*executablePath: 
       process.env.NODE_ENV === "production"
       ? process.env.PUPPERTEER_EXECUTABLE_PATH 
-      : puppeteer.executablePath(),
+      : puppeteer.executablePath(),*/
   });
 
   const page = await browser.newPage();
